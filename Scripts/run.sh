710e65089d91fd3bf61902c141cb185867e63c7f
bagrorg <54815354+bagrorg@users.noreply.github.com>
2021-12-23 03:09:49 +0000

    + main script

---
+++ Scripts/run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+python mergeGraphFiles.py $1 $2 $3 $4
+
+python dijkstra.py $1 $5 $8
+python dijkstra.py $2 $6 $8
+
+if [ -z "${11}" ]
+  then
+    ./$9 --algo ${10} --graph_path $4 --start_id $7 --goal_id $8
+  else
+    ./$9 --algo ${10} --graph_path $4 --start_id $7 --goal_id $8 --history_path ${11}
+fi
+
